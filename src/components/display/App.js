6c70e85283fad013bb17673c1df17ca8402303f3
Hrishabh Joshi <harishabh2019@iiitkottayam.ac.in>
2021-04-04 10:05:39 +0530

    scanner input build

--- src/components/display/App.js
+++ src/components/display/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 var QRCode = require('qrcode.react');
 
 function App() {
+    const [value, setValue] = useState('')
+    const [url, setUrl] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUrl(value)
+        setValue('')
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
     return (
         <div>
-            <QRCode value="http://facebook.github.io/react/" />
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Business:
+          <input type="text" value={value} onChange={handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            <QRCode value={url} />
         </div>
     )
 }
--- src/components/scan/App.js
+++ src/components/scan/App.js
@@ -24,7 +24,7 @@ function App() {
                 onScan={handleScan}
                 style={{ width: '100%' }}
             />
-            <p>{result}</p>
+            <a href={result}>{result}</a>
         </div>
     )
 }
